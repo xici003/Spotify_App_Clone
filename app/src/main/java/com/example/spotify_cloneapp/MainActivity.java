91a9b970949df1250e968493b0335a53a47a784b
xici003 <nqchi2003@gmail.com>
2024-03-18 22:51:28 +0700

    spotify_ui

--- app/src/main/java/com/example/spotify_cloneapp/MainActivity.java
+++ app/src/main/java/com/example/spotify_cloneapp/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.spotify_cloneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.spotify_cloneapp.Fragments.FavoriteFragment;
+import com.example.spotify_cloneapp.Fragments.HomeFragment;
+import com.example.spotify_cloneapp.Fragments.SearchFragment;
+import com.example.spotify_cloneapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int MENU_HOME_ID = R.id.mnuHome;
+    final int MENU_SEARCH_ID = R.id.mnuSearch;
+
+    final int MENU_FAVOURITE_ID = R.id.mnuFavorite;
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
+        // Bottom Navigation View
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if(itemId == MENU_HOME_ID){
+                replaceFragment(new HomeFragment());
+            }
+            if(itemId == MENU_SEARCH_ID){
+                replaceFragment(new SearchFragment());
+            }
+            if(itemId == MENU_FAVOURITE_ID){
+                replaceFragment(new FavoriteFragment());
+            }
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
